cae85af5381baf11c1360becd405399b3edc25dc
Jose Ruz <69995186+JoseSGW@users.noreply.github.com>
2021-05-27 22:19:09 -0400

    Add files via upload

---
+++ build/precache-manifest.0ee8e9dfebe6b5ab9e87a25d2dbf25aa.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f0c56bd6d118c0b58841907d2a34447",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f35a54163214b771efd",
+    "url": "/static/css/2.6b9de3f0.chunk.css"
+  },
+  {
+    "revision": "1417410b6236b03efef2",
+    "url": "/static/css/main.8d4b3670.chunk.css"
+  },
+  {
+    "revision": "7f35a54163214b771efd",
+    "url": "/static/js/2.9f6dc8c8.chunk.js"
+  },
+  {
+    "revision": "2ae89133620ce1ea6424bf4d6522872a",
+    "url": "/static/js/2.9f6dc8c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1417410b6236b03efef2",
+    "url": "/static/js/main.0f6fd972.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "5e901bb69dea05d2115ea1081b7ebf8c",
+    "url": "/static/media/Banner-1.5e901bb6.png"
+  },
+  {
+    "revision": "5fb778e7b453aa3bec83f2bc3e9472a2",
+    "url": "/static/media/Banner-2.5fb778e7.png"
+  },
+  {
+    "revision": "f9838e1aaca220f23f4a4a9e3b2a22be",
+    "url": "/static/media/Banner-3.f9838e1a.png"
+  },
+  {
+    "revision": "7cf598d14b33fc8c439fb1a7092e7da8",
+    "url": "/static/media/Banner-4.7cf598d1.png"
+  },
+  {
+    "revision": "ef3169489ea3893e968f4f5b837b336c",
+    "url": "/static/media/Banner-5.ef316948.png"
+  },
+  {
+    "revision": "0af33a0ed869251e282dc0451017010a",
+    "url": "/static/media/Fondo.0af33a0e.png"
+  },
+  {
+    "revision": "d845086eb8a0448d4d84d0790d881b26",
+    "url": "/static/media/Leo.d845086e.jpg"
+  },
+  {
+    "revision": "18df7cb2e2af02b9e90be468a609f59d",
+    "url": "/static/media/Magui.18df7cb2.jpg"
+  },
+  {
+    "revision": "5d062290bbd5bf4092e8875bf3aab073",
+    "url": "/static/media/Mari.5d062290.jpg"
+  },
+  {
+    "revision": "4627ab53aec5dcc5edec2f4967e4559b",
+    "url": "/static/media/Mau.4627ab53.jpg"
+  },
+  {
+    "revision": "2242d0b44e6fe4784c054cb9d9782aa0",
+    "url": "/static/media/cuchara.2242d0b4.png"
+  },
+  {
+    "revision": "14137f89247c1ab0eb29e8b75ef6e9d6",
+    "url": "/static/media/google-icon.14137f89.svg"
+  },
+  {
+    "revision": "73a57c642116a21903a9554278b8261b",
+    "url": "/static/media/soldOut.73a57c64.svg"
+  },
+  {
+    "revision": "29c0c6f2c5923ae8d4733e140547a6bf",
+    "url": "/static/media/te-amarillo.29c0c6f2.png"
+  },
+  {
+    "revision": "1852a6b0a3101a4ab12a1cf905048248",
+    "url": "/static/media/te-azul.1852a6b0.png"
+  },
+  {
+    "revision": "e34bea8fb82e731513c0bcb6b686eb41",
+    "url": "/static/media/te-blanco.e34bea8f.png"
+  },
+  {
+    "revision": "b61987d241733aa26ced15f3cc15f51f",
+    "url": "/static/media/te-negro.b61987d2.png"
+  },
+  {
+    "revision": "9c13839b6b94ed097ef0e0a558c53662",
+    "url": "/static/media/te-rojo.9c13839b.png"
+  },
+  {
+    "revision": "844906621e11d9883593da04ef171d91",
+    "url": "/static/media/te-verde.84490662.png"
+  }
+]);
